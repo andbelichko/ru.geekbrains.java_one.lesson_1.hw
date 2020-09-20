e76de82884fc8b256d81b4bcb1f63fb69dff47eb
andbelichko <andbelichko@gmail.com>
2020-09-20 21:32:54 +0400

    task5

--- src/ru/geekbrains/java/HomeWork/Animal.java
+++ src/ru/geekbrains/java/HomeWork/Animal.java
@@ -1,4 +1,23 @@
 package ru.geekbrains.java.HomeWork;
 
-public class Animal {
+public abstract class Animal {
+
+    protected String name;
+    protected String color;
+    protected int age;
+
+    public Animal(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+
+    }
+
+    abstract void run(double barrier);
+
+    abstract void swim(double barrier);
+
+    abstract void jamp(double barrier);
+
 }
+
--- src/ru/geekbrains/java/HomeWork/Cat.java
+++ src/ru/geekbrains/java/HomeWork/Cat.java
@@ -1,4 +1,33 @@
 package ru.geekbrains.java.HomeWork;
 
-public class Cat {
+public class Cat extends Animal {
+    static int stamina = 200;
+    static int jampHeight = 2;
+
+    Cat(String name, String color, int age){
+        super(name, color, age);
+    }
+
+    @Override
+   void run(double barrier){
+        if(stamina >= barrier){
+            System.out.println("Кот пробежал дистанцию");
+        } else{
+            System.out.println("Пробежав часть дистанции, кот устало мяукнул и пошёл по своим делам");
+        }
+    }
+    @Override
+    void swim(double barrier){
+        System.out.println("Кот зашипел и убежал прочь от воды");
+    }
+    @Override
+    void jamp(double barrier){
+        if(jampHeight >= barrier){
+            System.out.println("Кот перепрыгнул барьер");
+        } else{
+            System.out.println("Кот отказывается прыгать");
+        }
+    }
+
 }
+
