a6e09e6275320ee0304602f02bfd57ce454d243d
andbelichko <andbelichko@gmail.com>
2020-09-24 11:57:02 +0400

    task6

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -1,3 +1,34 @@
+import javafx.scene.chart.ScatterChart;
+
+import java.io.*;
+import java.util.Scanner;
+
 public class HomeWork {
 
+    public static void main(String[] args) {
+        gluing();
+
+    }
+
+    private static void gluing() {
+
+        try {
+            Scanner scanner = new Scanner(new FileInputStream("File2.txt"));
+            while (scanner.hasNext()) {
+                String text = scanner.next();
+
+
+                PrintStream ps = new PrintStream(new FileOutputStream("File1.txt", true));
+                ps.println(text);
+
+            }
+
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
 }
+
