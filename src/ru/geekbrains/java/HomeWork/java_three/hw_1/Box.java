bc200c92289906598c815efb8c6bd6885a2d22e7
andbelichko <andbelichko@gmail.com>
2020-11-14 20:32:31 +0300

    task1

--- src/ru/geekbrains/java/HomeWork/java_three/hw_1/Box.java
+++ src/ru/geekbrains/java/HomeWork/java_three/hw_1/Box.java
@@ -1,4 +1,43 @@
 package ru.geekbrains.java.HomeWork.java_three.hw_1;
 
-public class Box {
+import java.util.ArrayList;
+
+public class Box<T extends Fruit> {
+
+    ArrayList<Fruit> boxList = new ArrayList<>();
+
+    public Box() {
+
+    }
+
+    public void putFruit(Fruit fruit) {
+        boxList.add(fruit);
+    }
+
+    public double getWeight() {
+        double result = 0.0;
+        for (int i = 0; i < boxList.size(); i++) {
+           result += boxList.get(i).getFruitWeight();
+        }
+        return result;
+    }
+
+    public boolean compare(Box box) {
+        return this.getWeight() - box.getWeight() < 0.0001;
+    }
+
+    public void pourOver(Box<T> box) {
+        for (int i = 0; i < this.boxList.size(); i++) {
+            box.putFruit(this.boxList.get(i));
+        }
+        this.boxList.removeAll(boxList);
+    }
+
+    public void toStr() {
+        System.out.println(boxList);
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/ru/geekbrains/java/HomeWork/java_three/hw_1/Orange.java
+++ src/ru/geekbrains/java/HomeWork/java_three/hw_1/Orange.java
@@ -1,4 +1,7 @@
 package ru.geekbrains.java.HomeWork.java_three.hw_1;
 
-public class Orange {
+public class Orange extends Fruit{
+    public Orange() {
+        this.weight = 1.5;
+    }
 }
