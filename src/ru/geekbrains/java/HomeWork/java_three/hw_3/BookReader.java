19380b7d13ad4418ce5841b877e3a43e26216509
andbelichko <andbelichko@gmail.com>
2020-11-22 14:29:02 +0300

    task3

--- src/ru/geekbrains/java/HomeWork/java_three/hw_3/BookReader.java
+++ src/ru/geekbrains/java/HomeWork/java_three/hw_3/BookReader.java
@@ -1,4 +1,37 @@
 package ru.geekbrains.java.HomeWork.java_three.hw_3;
 
+import java.io.*;
+
 public class BookReader {
+    public static void main(String[] rgs) {
+        //task3
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            final int PAGE_SIZE = 1800;
+            int page = 0;
+            System.out.println("Укажите путь к книге");
+            String path = reader.readLine();
+
+            while (true) {
+                System.out.println("\nВведите номер страницы, для выхода введите 0");
+                page = Integer.parseInt(reader.readLine()) - 1;
+                if (page == -1) break;
+
+                reedBook(path, page, PAGE_SIZE);
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public static void reedBook(String path, int page, int PAGE_SIZE) {
+        try(RandomAccessFile raf = new RandomAccessFile(path, "r")) {
+            raf.seek(page * PAGE_SIZE);
+            for (int i = 0; i < PAGE_SIZE; i++) {
+                System.out.print((char) raf.read());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/ru/geekbrains/java/HomeWork/java_three/hw_3/Main.java
+++ src/ru/geekbrains/java/HomeWork/java_three/hw_3/Main.java
@@ -1,4 +1,44 @@
 package ru.geekbrains.java.HomeWork.java_three.hw_3;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
+    public static void main(String[] args) {
+        //task1
+        byte[] byteArr = new byte[512];
+        try (FileInputStream in = new FileInputStream("Kubla Khan.txt")) {
+            int count;
+            while ((count = in.read(byteArr)) > 0) {
+                System.out.print(new String(byteArr, 0, count, "UTF-8"));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //task2
+        ArrayList<InputStream> al = new ArrayList<>();
+        try {
+            al.add(new FileInputStream("Yuggoth1.txt"));
+            al.add(new FileInputStream("Yuggoth2.txt"));
+            al.add(new FileInputStream("Yuggoth3.txt"));
+            al.add(new FileInputStream("Yuggoth4.txt"));
+            al.add(new FileInputStream("Yuggoth5.txt"));
+
+            SequenceInputStream in = new SequenceInputStream(Collections.enumeration(al));
+            FileOutputStream out = new FileOutputStream("Yuggoth1-5.txt");
+
+            byte[] arr = new byte[512];
+            int count;
+            while ((count = in.read()) != -1) {
+                out.write(count);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
