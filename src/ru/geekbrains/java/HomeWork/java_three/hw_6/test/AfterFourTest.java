5e1239810987562b1c27b89a793fab4c67961a7c
andbelichko <andbelichko@gmail.com>
2020-12-05 14:33:42 +0300

    task6

--- src/ru/geekbrains/java/HomeWork/java_three/hw_6/test/AfterFourTest.java
+++ src/ru/geekbrains/java/HomeWork/java_three/hw_6/test/AfterFourTest.java
@@ -1,4 +1,46 @@
 package ru.geekbrains.java.HomeWork.java_three.hw_6.test;
 
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import ru.geekbrains.java.HomeWork.java_three.hw_6.AfterFour;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class AfterFourTest {
+    @Parameterized.Parameters
+    public static Collection<Object[][]> data() {
+        return Arrays.asList(new Object[][][] {
+                {{4, 2, 5, 9, 11, 23, 12, 0, 34, 3, 6}, {2, 5, 9, 11, 23, 12, 0, 34, 3, 6}},
+                {{1, 63, 5, 9, 25, 8, 12, 0, 6, 3, 4}, {}},
+                {{7, 2, 5, 25, 6, 4, 12, 0, 34, 3, 6}, {12, 0, 34, 3, 6}}
+        });
+    }
+
+    private int[] arrTest;
+    private Integer[] arrExc;
+    private AfterFour afterFour;
+    public AfterFourTest(int[] arrTest, Integer[] arrExc) {
+        this.arrTest = arrTest;
+        this.arrExc = arrExc;
+    }
+
+
+    @Before
+    public void init () {
+        afterFour = new AfterFour();
+    }
+
+    @Test
+    public void test1 () {
+        Assert.assertArrayEquals(arrExc, afterFour.afterFour4(arrTest));
+    }
 }
+
+
--- src/ru/geekbrains/java/HomeWork/java_three/hw_6/test/ArrayCheckTest.java
+++ src/ru/geekbrains/java/HomeWork/java_three/hw_6/test/ArrayCheckTest.java
@@ -1,4 +1,44 @@
 package ru.geekbrains.java.HomeWork.java_three.hw_6.test;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import ru.geekbrains.java.HomeWork.java_three.hw_6.ArrayCheck;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class ArrayCheckTest {
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                {true, new int[]{2,1,4,4,1,1}},
+                {true, new int[]{4,4,4,4,4}},
+                {true, new int[]{1,1}},
+                {true, new int[]{1,1,4,4,4,4,1,1}},
+        });
+    }
+
+    private ArrayCheck arrCh;
+    private boolean a;
+    private int[] b;
+
+    public ArrayCheckTest(boolean a, int[] b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Before
+    public void init() {
+        arrCh = new ArrayCheck();
+    }
+
+    @Test
+    public void testTask2() {
+        Assert.assertEquals(a, arrCh.ArrayCheck4And1(b));
+    }
 }
+
